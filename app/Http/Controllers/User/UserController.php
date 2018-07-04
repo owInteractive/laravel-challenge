78fa6661cca419be36e60206602d057282f95285
Nicolas Anelli <nickanelli@hotmail.com>
2018-07-04 00:17:25 -0300

    Creating users endpoint for api route, using Requests

---
+++ app/Http/Controllers/User/UserController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Requests\UpdateUserRequest;
+use App\Models\User;
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $users = User::all();
+
+        return response()->json($users, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreUserRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreUserRequest $request)
+    {
+        $data = $request->only([
+            'name',
+            'email',
+        ]);
+        $data['password'] = bcrypt($request->input('password'));
+
+        $user = User::create($data);
+
+        return response()->json($user, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        return response()->json($user, 200);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateUserRequest  $request
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateUserRequest $request, User $user)
+    {
+        $data = $request->all();
+        if ($request->has('password'))
+            $data['password'] = bcrypt($request->input('password'));
+
+        $user->fill($data);
+
+        $user->save();
+
+        return response()->json($user, 200);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(User $user)
+    {
+        $user->delete();
+
+        return response()->json()->setStatusCode(204);
+    }
+}
