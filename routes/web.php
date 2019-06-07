550030bc60d256241c4e14f3c7cce86b965e2aa5
Arthur Menegoli <arthur@com2.com.br>
2019-06-07 11:18:06 -0300

    Authentication: Config auth classes, views and middlewares created after "php artisan make:auth"

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,12 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/', function () {
+        return view('welcome');
+    });
+    });
