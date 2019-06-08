2a535380fa22c5639749ac1456962056767c6598
Gabriel Cabral <cabral.9santos@gmail.com>
2019-06-08 02:28:47 -0300

    calendar: Criando migrations de Eventos, juntamente com a seed e a factory

---
+++ database/factories/EventFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| Here you may define all of your model factories. Model factories give
+| you a convenient way to create models for testing and seeding your
+| database. Just tell the factory how a default model should look.
+|
+*/
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Event;
+use Faker\Generator;
+
+$factory->define(Event::class, function (Faker\Generator $faker) {
+
+    $date_start = $faker->dateTimeBetween('this week', '+6 days');
+    $date_end = $faker->dateTimeBetween($date_start, strtotime('+6 days'));
+
+    return [
+        'title' =>  $faker->sentence(3),
+        'description' => $faker->paragraph,
+        'date_start' => $date_start,
+        'date_end' => $date_end
+    ];
+});
