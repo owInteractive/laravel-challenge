db8237472aa8ae75c3806732585fc7135b31ea59
Arthur Menegoli <arthur@com2.com.br>
2019-06-09 16:11:10 -0300

    Import and Export events data on CSV files

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,20 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-Route::group(['middleware' => 'auth'], function() {
-    Route::get('/', function () {
-        return view('event.list');
-    });
-    Route::resource('event', 'EventController');
-    });
+Route::group(['middleware' => 'auth'], function () {
+    Route::get('/', 'EventController@index');
+    //Events CRUD
+    Route::get('event', 'EventController@index')->name('event.index');
+    Route::get('event/{id}', 'EventController@show')->name('event.show');
+    Route::get('event/filter/{filter}', 'EventController@filter')->name('event.filter');
+    Route::post('event', 'EventController@store')->name('event.store');
+    Route::delete('event/{filter}', 'EventController@destroy')->name('event.destroy');
+    //CSV
+    Route::get('import', 'ExcelController@import')->name('import');
+    Route::get('export', 'ExcelController@exportAll')->name('export.all');
+    Route::get('export/{id}', 'ExcelController@exportSingle')->name('export.single');
+    Route::post('import', 'ExcelController@importExcel')->name('import');
+    //Invite
+    Route::get('invite/{id}', 'InviteController@invite')->name('invite');
+    Route::post('invite/{id}', 'InviteController@mail')->name('invite');
+});
