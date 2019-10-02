e6d3653f4bca802fa4f213056446dc0fa2a8271b
krdg10 <krdg10@live.com>
2019-10-02 10:07:03 -0400

    separação do layout

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,85 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-</head>
+    @include('layouts.head')
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light py-3" id="sideNav">
-    <div class="container">
-      <a class="nav-link js-scroll-trigger" href="{{ url('/') }}">Pacotes</a>
-      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-        <ul class="navbar-nav ml-auto my-2 my-lg-0">
-            <li class="nav-item dropdown">
-                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                    Eventos <span class="caret"></span>
-                </a>
-                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                    <a class="dropdown-item" href="{{ url('/events') }}">
-                        Eventos de Hoje
-                    </a>
-                    <a class="dropdown-item" href="{{ url('/eventsNext') }}">
-                        Eventos próximos cinco dias
-                    </a>
-                    <a class="dropdown-item" href="{{ url('/allEvents') }}">
-                        Todos os Eventos
-                    </a>     
-                </div>
-            </li>
-          <li class="nav-item">
-            <a class="nav-link js-scroll-trigger" href="{{ url('/pacotes') }}">Pacotes</a>
-          </li>
-        
-          @if (Auth::guest())
-                            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ route('login') }}">Login</a></li>
-                            <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ route('register') }}">Register</a></li>
-                        @else
-                            <li class="nav-item dropdown">
-                                <a href="#" class="dropdown-toggle nav-link js-scroll-trigger" data-toggle="dropdown" role="button" aria-expanded="false">
-                                    {{ Auth::user()->name }} <span class="caret"></span>
-                                </a>
-
-                                <ul class="dropdown-menu" role="menu">
-                                    <li class="nav-item">
-                                        <a href="{{ route('logout') }}"
-                                            onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();"
-                                                     class="nav-link js-scroll-trigger">
-                                            Logout
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                            {{ csrf_field() }}
-                                        </form>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link js-scroll-trigger" href="{{ url('/myEvents') }}">Meus Eventos</a>
-                                    </li>
-                                    <li class="nav-item">
-                                        <a class="nav-link js-scroll-trigger" href="{{ url('/newEvents') }}">Criar Evento</a>
-                                    </li>
-                                </ul>
-                            </li>
-                        @endif
-        </ul>
-      </div>
-    </div>
-</nav>
+    
        
-
-        @yield('content')
+    @include('layouts.nav')
+    @yield('content')
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
---
+++ resources/views/layouts/nav.blade.php
@@ -0,0 +1,68 @@
+<nav class="navbar navbar-expand-lg navbar-light py-3" id="sideNav">
+    <div class="container">
+      <a class="nav-link js-scroll-trigger" href="{{ url('/') }}">Events</a>
+      <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarResponsive">
+        <ul class="navbar-nav ml-auto my-2 my-lg-0">
+            
+          <li class="nav-item">
+            <a class="nav-link js-scroll-trigger" href="{{ url('/pacotes') }}">Pacotes</a>
+          </li>
+        
+          @if (Auth::guest())
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ route('login') }}">Login</a></li>
+                <li class="nav-item"><a class="nav-link js-scroll-trigger" href="{{ route('register') }}">Register</a></li>
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                        Eventos <span class="caret"></span>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="{{ url('/events') }}">
+                            Eventos de Hoje
+                        </a>
+                        <a class="dropdown-item" href="{{ url('/eventsNext') }}">
+                            Eventos próximos cinco dias
+                        </a>
+                        <a class="dropdown-item" href="{{ url('/allEvents') }}">
+                            Todos os Eventos
+                        </a>     
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                    <a href="#" class="dropdown-toggle nav-link js-scroll-trigger" data-toggle="dropdown" role="button" aria-expanded="false">
+                        {{ Auth::user()->name }} <span class="caret"></span>
+                    </a>
+
+                    <ul class="dropdown-menu" role="menu">
+                        
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="{{ url('/myEvents') }}">Meus Eventos</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="{{ url('/newEvents') }}">Criar Evento</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link js-scroll-trigger" href="{{ url('/myProfile') }}">Meu Perfil</a>
+                        </li>
+                        <li class="nav-item">
+                            <a href="{{ route('logout') }}"
+                                onclick="event.preventDefault();
+                                            document.getElementById('logout-form').submit();"
+                                            class="nav-link js-scroll-trigger">
+                                Logout
+                            </a>
+
+                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                {{ csrf_field() }}
+                            </form>
+                        </li>
+                    </ul>
+                </li>
+            @endif
+        </ul>
+      </div>
+    </div>
+</nav>
