ce965b97d41d4110ada0a04d0395bd0604eb5414
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-01-10 03:59:03 -0300

    Displaying username on navbar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,18 +26,22 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
-            <ul class="navbar-nav mr-auto">
-                <li class="nav-item active">
-                    <a class="nav-link" href="/">Dashboard</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Import / Export</a>
-                </li>
-            </ul>
+            @if (Auth::check())
 
-            <a href="#">
-                <small>Username</small>
-            </a>
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item active">
+                        <a class="nav-link" href="/">Dashboard</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Import / Export</a>
+                    </li>
+                </ul>
+
+                <a href="#">
+                    <small>{{Auth::user()->name}}</small>
+                </a>
+
+            @endif
 
         </div>
     </nav>
