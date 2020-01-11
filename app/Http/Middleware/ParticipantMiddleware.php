00b1244a7ecf05268accb429f63e8a5e117a3dcd
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-01-11 03:27:45 -0300

    Creating middleware to protect events for its participants

---
+++ app/Http/Middleware/ParticipantMiddleware.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Event;
+use Closure;
+use Illuminate\Contracts\Auth\Guard;
+
+class ParticipantMiddleware
+{
+
+    protected $auth;
+
+    public function __construct(Guard $auth)
+    {
+        $this->auth = $auth;
+    }
+
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        $eventId = $request->segments()[1];
+        $event = Event::find($eventId);
+
+        if (!is_a($event, Event::class)) {
+            return redirect('/')
+                ->withErrors('This event could not be found.');
+        }
+
+        if ($event->user->id !== $this->auth->getUser()->id) {
+            return redirect('/')
+                ->withErrors('This event could not be found.');
+        }
+
+        return $next($request);
+
+    }
+
+}
