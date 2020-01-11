4e94204aaafd6cd60024635bc8ad107c16a0c3b3
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-01-11 02:40:32 -0300

    Displaying erros and messages on the global layout

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -55,6 +55,29 @@
     </nav>
 
     <div class="container mb-3">
+
+        @if ($errors->any())
+            <div class="alert alert-danger alert-dismissible fade show mt-3" role="alert">
+                <ul class="mb-0">
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+
+        @if (session('success'))
+            <div class="alert alert-success alert-dismissible fade show mt-3" role="alert">
+                {{ session('success') }}
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+        @endif
+
         @yield('content')
     </div>
 
