27dcaf81e5a4b062d4960b642549eee1f0113218
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-01-12 23:57:50 -0300

    Setting up Log as default Mail Driver, and Sqlite as default database

--- config/database.php
+++ config/database.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => 'sqlite',
+    'default' => env('DB_CONNECTION', 'sqlite'),
 
     /*
     |--------------------------------------------------------------------------
@@ -35,7 +35,7 @@ return [
 
         'sqlite' => [
             'driver' => 'sqlite',
-            'database' => database_path('database.sqlite'),
+            'database' => env('DB_DATABASE', database_path('database.sqlite')),
             'prefix' => '',
         ],
 
