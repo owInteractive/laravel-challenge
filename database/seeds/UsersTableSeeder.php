d944e5637d21c62938bc4992d25dbf12f7476469
Newton Scavazzini <newtonscavazzini@gmail.com>
2020-01-18 04:08:05 -0300

    Updating database seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,8 +1,9 @@
 <?php
 
+use Carbon\Carbon;
+use Faker\Factory;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Str;
 
 class UsersTableSeeder extends Seeder
 {
@@ -10,13 +11,48 @@ class UsersTableSeeder extends Seeder
      * Run the database seeds.
      *
      * @return void
+     * @throws Exception
      */
     public function run()
     {
-        DB::table('users')->insert([
-            'name' => Str::random(10),
-            'email' => Str::random(10).'@gmail.com',
-            'password' => bcrypt('password'),
-        ]);
+
+        $faker = Factory::create();
+
+        DB::beginTransaction();
+
+        try {
+
+            $userId = DB::table('users')->insertGetId([
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => bcrypt('password'),
+                "created_at" =>  Carbon::now(),
+                "updated_at" => Carbon::now(),
+            ]);
+
+            $eventId = DB::table('events')->insertGetId([
+                'title' => $faker->sentence,
+                'description' => $faker->text,
+                'start_at' => Carbon::today(),
+                'end_at' => Carbon::today()->addDays(7),
+                "created_at" =>  Carbon::now(),
+                "updated_at" => Carbon::now(),
+            ]);
+
+            DB::table('event_user')->insert([
+                'event_id' => $eventId,
+                'user_id' => $userId,
+                'owner' => true,
+                "created_at" =>  Carbon::now(),
+                "updated_at" => Carbon::now(),
+            ]);
+
+            DB::commit();
+
+        } catch (Exception $e) {
+            DB::rollback();
+            throw $e;
+        }
+
     }
 }
