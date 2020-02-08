f0cb69ad2a91025dcc2ec89699d97267f0f4e2ec
Vinicius Almeida <vinicius.almeida@gruponewway.com.br>
2020-02-08 12:43:40 -0300

    Create Events Controller

---
+++ app/Http/Controllers/EventsController.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Business\EventsBusiness;
+use Illuminate\Http\Request;
+
+class EventsController extends Controller
+{
+    private $eventsBusiness;
+
+    public function __construct(EventsBusiness $eventsBusiness)
+    {
+        $this->eventsBusiness = $eventsBusiness;
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Events  $events
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Events $events)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Events  $events
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Events $events)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Events  $events
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Events $events)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Events  $events
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Events $events)
+    {
+        //
+    }
+}
