441f3c2314a79344e48eab10295a7c796acfde9a
Gustavo <gustavo.softdev@gmail.com>
2020-02-09 14:23:18 -0200

    Created events list view

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -2,14 +2,57 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <div class="col-xs-12 col-sm-4 col-md-4 col-lg-3">
-                <div class="event" title="Create">
-                    <div class="background" style="background: url('data:image/jpg;base64, {{$data['image']}}');background-position: center;background-size: cover;">
-                    </div>
-                    <div class="overlay">
-                        <div class="buttons">
-                            <span><a href="{{route('event-new')}}"><i class="fa fa-plus"></i></a></span>
+        @if($errors->any())
+            <div class="callout">
+                <h4>Oops</h4>
+                <p>{{$errors->first()}}</p>
+            </div>
+        @endif
+        <div class="page-title">
+            <span class="page-title-text text-primary">My Events</span>
+            <button onClick="window.open('{{route('event-new')}}', '_parent')" class="btn btn-primary btn-flat pull-right">Add new event</button>
+        </div>
+    </div>
+    <div class="container">
+        <div class="content-box">
+            <div class="box-header with-border">
+                <h3 class="box-title">List of all events</h3>
+            </div>
+            <div class="box-body">
+                <div class="row">
+                    <div class="col-xs-12">
+                        <div class="table-responsive">
+                            <table class="table table-hover table-striped table-condensed table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Title</th>
+                                        <th>Start Date</th>
+                                        <th>Finish Date</th>
+                                        <th>Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach ($events as $event)
+                                        <tr id="row{{$event['id']}}">
+                                            <td>{{$event['id']}}</td>
+                                            <td>{{$event['title']}}</td>
+                                            <td>{{date('m/d/Y H:i', strtotime($event['start_date']))}}</td>
+                                            <td>{{date('m/d/Y H:i', strtotime($event['finish_date']))}}</td>
+                                            <td class="icons-col">
+                                                <a href="{{route('event-new')}}"><i class="fa fa-eye"></i></a>
+                                                <a href="/event/{{\Crypt::encryptString($event['id'])}}/update"><i class="fa fa-pencil"></i></a>
+                                                <i class="fa fa-trash"></i>
+                                            </td>
+                                        </tr>
+                                    @endforeach
+                                </tbody>
+                            </table>
+                        </div>  
+                        <div class="row">
+                            <div class="col-xs-12">
+                                {{$events->render()}}
+                            </div>
                         </div>
                     </div>
                 </div>
