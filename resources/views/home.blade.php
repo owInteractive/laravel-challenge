1518ff2d959fcce75987532f5f0f00d78aed347f
Gustavo <gustavo.softdev@gmail.com>
2020-02-09 13:19:17 -0200

    Home first layout

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,16 +2,51 @@
 
 @section('content')
 <div class="container">
-    <div class="row">
-        <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
-                <div class="panel-body">
-                    You are logged in!
+   <div class="row">
+       <div class="col-xs-12 col-md-3">
+           <div class="box box-blue">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>New access</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-plus"></i>
+                </div>
+           </div>
+       </div>
+       <div class="col-xs-12 col-md-3">
+            <div class="box box-green">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>Confirmed</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-check"></i>
                 </div>
             </div>
         </div>
-    </div>
+        <div class="col-xs-12 col-md-3">
+            <div class="box box-orange">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>Interested</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-question"></i>
+                </div>
+            </div>
+        </div>
+        <div class="col-xs-12 col-md-3">
+            <div class="box box-red">
+                <div class="inner">
+                    <h3>0</h3>
+                    <p>Denied</p>
+                </div>
+                <div class="icon">
+                    <i class="fa fa-times"></i>
+                </div>
+            </div>
+        </div>
+   </div>
 </div>
 @endsection
