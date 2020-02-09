0f6e1b42e991297801887d4578c20b27767ea720
Gustavo <gustavo.softdev@gmail.com>
2020-02-09 14:22:48 -0200

    Added user_id on fillable and decryptId static function

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,12 +3,21 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Crypt;
 
 class Event extends Model
 {
     protected $fillable = [
-        'title', 'description', 'start_date', 'finish_date'
+        'title', 'user_id', 'description', 'start_date', 'finish_date'
     ];
 
     protected $table = "events";
+
+    static function decryptId($id) {
+        try {
+            return Crypt::decryptString($id);
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
 }
