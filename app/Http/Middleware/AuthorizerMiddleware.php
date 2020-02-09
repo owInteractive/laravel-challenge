962ec80d42a0768a34acad8cc69a3d00096458c1
Vinicius Almeida <vinicius.almeida@gruponewway.com.br>
2020-02-09 11:49:17 -0300

    Create Authorizer Middleware

---
+++ app/Http/Middleware/AuthorizerMiddleware.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Auth;
+
+class AuthorizerMiddleware
+{
+    public function handle($request, Closure $next)
+    {
+        if (!$request->is('login') && Auth::guest()) {
+            return redirect('login');
+        }
+
+        return $next($request);
+    }
+}
