fc8c637776d6a2e1ec3cf49a2d50d09d1b67fec7
Vinicius Almeida <vinicius.almeida@gruponewway.com.br>
2020-02-10 22:09:45 -0300

    Add method to format date from form

--- app/Models/Events.php
+++ app/Models/Events.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Events extends Model
@@ -22,5 +23,18 @@ class Events extends Model
     public function participants()
     {
         return $this->belongsToMany(User::class, 'event_user');
+    }
+
+    public function formatDate($date)
+    {
+        if (is_null($date)) {
+            return null;
+        }
+
+        try {
+            return Carbon::parse($date)->format('d-m-Y H:i:s');
+        } catch (\Exception $ex) {
+            return null;
+        }
     }
 }
