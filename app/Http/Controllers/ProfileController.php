d7d4cb1a202ad066236036861b0f4d3839d74f28
Vinicius Almeida <vinicius.almeida@gruponewway.com.br>
2020-02-11 23:29:26 -0300

    Create ProfileController

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Business\UserBusiness;
+use App\Http\Requests\ProfileChangePasswordFormRequest;
+use App\Http\Requests\ProfileUpdateFormRequest;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
+class ProfileController extends Controller
+{
+    private $userBusiness;
+
+    public function __construct(UserBusiness $userBusiness)
+    {
+        $this->userBusiness = $userBusiness;
+    }
+
+    public function index()
+    {
+        return view('profiles.index');
+    }
+
+    public function update(ProfileUpdateFormRequest $request)
+    {
+        $this->userBusiness->update($request->all());
+        return redirect('profile/index');
+    }
+
+    public function changePassword(ProfileChangePasswordFormRequest $request)
+    {
+        $user = Auth::user();
+        $currentPassword = $request->get('current_password');
+        if (!(Hash::check($currentPassword, $user->password))) {
+            info("aqui");
+            return redirect('profile/index')
+                ->withErrors(['Your current password must match with the password you provide.']);
+        }
+
+        $this->userBusiness->update($request->all());
+        return redirect('profile/index');
+    }
+
+}
