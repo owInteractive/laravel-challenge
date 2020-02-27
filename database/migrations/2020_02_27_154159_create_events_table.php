f47461bf68806a81bcc7fbc0128ba811903020dd
T1B4 <fernando.tiburcio@gmail.com>
2020-02-27 12:56:13 -0300

    Adicionado migrations, controllers e models de Eventos e Convidados

---
+++ database/migrations/2020_02_27_154159_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned()->default(12);
+            $table->integer('guest_id')->unsigned()->nullable()->default(12);
+            $table->string('event_title');
+            $table->string('event_description');
+            $table->dateTime('start', 0);
+            $table->dateTime('end', 0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
