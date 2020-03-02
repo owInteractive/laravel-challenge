16d1ca434de11ca885100695d90c973d27982d83
T1B4 <fernando.tiburcio@gmail.com>
2020-03-02 10:20:37 -0300

    Envio projeto parcial

--- public/js/app.js
+++ public/js/app.js
@@ -32564,8 +32564,8 @@ if (token) {
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\xampp\htdocs\laravel-challenge\resources\assets\js\app.js */"./resources/assets/js/app.js");
-module.exports = __webpack_require__(/*! C:\xampp\htdocs\laravel-challenge\resources\assets\sass\app.scss */"./resources/assets/sass/app.scss");
+__webpack_require__(/*! /var/www/laravel-challenge/resources/assets/js/app.js */"./resources/assets/js/app.js");
+module.exports = __webpack_require__(/*! /var/www/laravel-challenge/resources/assets/sass/app.scss */"./resources/assets/sass/app.scss");
 
 
 /***/ })
