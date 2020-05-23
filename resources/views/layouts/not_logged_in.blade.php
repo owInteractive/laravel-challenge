1b10234516581dc088ab652dc83bf884d01d35fb
Rafael Breno <rafael_breno@outlook.com>
2020-05-23 19:31:23 -0300

    Aligning horizontally every auth card

--- resources/views/layouts/not_logged_in.blade.php
+++ resources/views/layouts/not_logged_in.blade.php
@@ -1,2 +1,8 @@
 @include("partials.navbar")
-@yield("content")
+<div class="container-fluid">
+    <div class="row mt-3">
+        <div class="col-lg-6 col-md-8 col-sm-12 mx-auto">
+            @yield("content")
+        </div>
+    </div>
+</div>
