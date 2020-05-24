18efa3a8c37a1a7330b05a59c93c96f57f1aea80
Rafael Breno <rafael_breno@outlook.com>
2020-05-24 17:24:27 -0300

    Created "event" dictionary

---
+++ resources/lang/en/event.php
@@ -0,0 +1,13 @@
+<?php
+return [
+    'create' => [
+        'title' => 'Define a new Event',
+        'register' => 'Create Event'
+    ],
+    'form' => [
+        'title' => 'Title:',
+        'description' => 'Description',
+        'start_date' => 'Beginning Date:',
+        'end_date' => 'Ending Date:'
+    ]
+];
