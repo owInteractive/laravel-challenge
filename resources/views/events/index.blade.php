56f0832edc0098855a694208701866a733c0a9e3
Rafael Breno <rafael_breno@outlook.com>
2020-05-24 19:04:09 -0300

    Created a index file to list all user's events

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -1,4 +1,30 @@
 @extends("layouts.app")
+
+@section("card-header")
+    {{ __("event.list.title") }}
+@endsection
+
 @section("card-body")
-    <h1>Salve Cachorrada</h1>
+    <table class="table">
+        <thead>
+        <tr>
+            <th>id</th>
+            <th>title</th>
+            <th>desc</th>
+        </tr>
+        </thead>
+        <tbody>
+        @foreach($events as $event)
+            <tr>
+                <td scope="row">{{ $event['id'] }}</td>
+                <td>{{ $event['title'] }}</td>
+                <td>{{ $event['description'] }}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+@endsection
+
+@section("card-footer")
+
 @endsection
