7e618f060f1e09a0c38939b68c4ce136567d42e7
Rafael Breno <rafael_breno@outlook.com>
2020-05-25 20:21:04 -0300

    Fixed alert "not closing" issue, added jQuery function "element.hide()"

--- resources/views/partials/alert.blade.php
+++ resources/views/partials/alert.blade.php
@@ -1,13 +1,11 @@
-<div class="position-absolute min-vw-100">
+<div class="position-absolute min-vw-100" id="alert-div">
     @if(session('success'))
-        <div class="alert alert-success alert-dismissible fade show m-3 mx-auto w-25"  style="z-index: 999" role="alert">
-            <strong>Success</strong> {{ __(session('success')) }}
+        <div class="alert alert-success alert-dismissible fade show m-3 mx-auto w-25"  style="z-index: 999" role="alert"><strong>Success</strong> {{ __(session('success')) }}
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
             </button>
         </div>
-    @endif
-    @if(session('error'))
+    @elseif(session('error'))
         <div class="alert alert-danger alert-dismissible fade show m-3 mx-auto w-25"  style="z-index: 999" role="alert">
             <strong>Error!</strong> {{ __(session('error')) }}
             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
@@ -16,3 +14,12 @@
         </div>
     @endif
 </div>
+
+
+<script>
+    $(document).ready(function () {
+        $('button[id="buttonClose"]').click(function () {
+            $('div[id="alert-div"]').hide();
+        })
+    })
+</script>
