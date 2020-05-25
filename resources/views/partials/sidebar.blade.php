fbf4991f0114fa01af47549b0e0d0a85d58f8335
Rafael Breno <rafael_breno@outlook.com>
2020-05-25 19:31:28 -0300

    Added Link to our Export/Import Events

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -1,7 +1,5 @@
 <div class="list-group border-0">
-    <a href="{{ url('') }}" class="list-group-item bg-transparent border-0 border-bottom">
-        {{ env('APP_NAME') }}
-    </a>
     <a href="{{ url('events') }}" class="list-group-item bg-transparent border-0 border-bottom">List Events</a>
     <a href="{{ url('events/create') }}" class="list-group-item bg-transparent border-0 border-bottom">Create Events</a>
+    <a href="{{ url('exporter') }}" class="list-group-item bg-transparent border-0 border-bottom">Export/Export Events</a>
 </div>
