13abda97b4880346e2c566a4c60f33bafcaee783
Rafael Breno <rafael_breno@outlook.com>
2020-05-25 22:18:16 -0300

    Added Invite create/list routes

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -21,8 +21,10 @@
     <a href="{{ url('events/create') }}" class="list-group-item classe bg-transparent">
         <i class="fas fa-calendar-plus mr-1"></i>Create Events
     </a>
-
     <a href="{{ url('exporter') }}" class="list-group-item classe bg-transparent position-static">
+        <i class="fas fa-cloud-upload-alt mr-1"></i>Import/Export Events
+    </a>
+    <a href="{{ url('/invites') }}" class="list-group-item classe bg-transparent position-static">
         <i class="fas fa-cloud-upload-alt mr-1"></i>Import/Export Events
     </a>
 </div>
