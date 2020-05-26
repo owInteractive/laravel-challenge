a07d937c62d304d461db95dede3548c61e231193
Rafael Breno <rafael_breno@outlook.com>
2020-05-25 22:17:03 -0300

    Created Invitation system, only Create completed

---
+++ app/Http/Controllers/Tools/InvitationController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers\Tools;
+
+use App\Models\Event;
+use App\Models\Invite;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+
+class InvitationController extends Controller
+{
+    public function index()
+    {
+        $invites = Invite::all()->where();
+        return view('invite.index');
+    }
+    public function hash($id)
+    {
+        $str = $id . random_bytes(8);
+        return bin2hex($str);
+    }
+    public function create($id)
+    {
+        $hash = $this->hash($id);
+        $invite = Invite::create([
+            'user_id' => 1,
+            'event_id' => $id,
+            'hash' => $hash
+        ]);
+        return response()->json([
+            "data" => $invite
+        ],200);
+    }
+
+    public function getEventId($hash)
+    {
+        return Invite::all()->where('hash', $hash)->first()->event_id;
+    }
+    public function getEventDetails($hash)
+    {
+        $id = $this->getEventId($hash);
+        return Event::find($id)->toArray();
+    }
+    public function participate($hash)
+    {
+        $event = $this->getEventDetails($hash);
+        dd($event);
+    }
+
+}
---
+++ app/Models/Invite.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Invite extends Model
+{
+    protected $table = "invites";
+
+    protected $fillable = [
+        'user_id', 'event_id', 'hash'
+    ];
+}
