fc14af5159e7805f850710c545c15341ce6cd0d8
Rafael Breno <rafael_breno@outlook.com>
2020-05-25 22:18:43 -0300

    Added column to create an invitation

--- resources/views/events/index.blade.php
+++ resources/views/events/index.blade.php
@@ -12,6 +12,7 @@
             <th>Owner</th>
             <th>Title</th>
             <th>Start Date</th>
+            <th>Action</th>
         </tr>
         </thead>
         <tbody>
@@ -26,6 +27,7 @@
                 <td>{{ \App\Models\User::find($event['user_id'])->name }}</td>
                 <td>{{ $event['description'] }}</td>
                 <td><div id="endDate">{{ date_format(date_create_from_format('Y-m-d H:i:s', $event['end_date']), "H:i d/m/Y") }}</div></td>
+                <td><button class="btn btn-warning mx-1" value="{{ $event['id'] }}" id="createInvite"><i class="fas fa-envelope-open"></i></button></td>
             </tr>
         @endforeach
         </tbody>
@@ -37,6 +39,23 @@
                 columnDefs: [
                     { "orderable": false, "targets": 0 }
                 ]
+            });
+            $('button[id="createInvite"]').click(function () {
+                $.ajax({
+                    url: 'invite/create/' + $(this).val(),
+                    method: "POST",
+                    data: { },
+                    contentType: 'application/json',
+                    success: function (response) {
+                        alert("New invite link: http://localhost:8000/enter/" + response.data.hash);
+                    },
+                    error: function (response) {
+                        console.log(response);
+                        alert("Wasn't possible create an InvitationController, try again later!");
+                    }
+                }).done(function (response) {
+                    console.log(response);
+                });
             });
         });
     </script>
