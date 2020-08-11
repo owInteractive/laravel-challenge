c99b681e296b6a079cc39bd43c0166957338845c
Jclavo <jclavotafur@gmail.com>
2020-08-10 23:28:01 -0300

    Create Factory and seeder for Event Table

--- database/factories/EventFactory.php
+++ database/factories/EventFactory.php
@@ -4,9 +4,14 @@
 
 use App\Event;
 use Faker\Generator as Faker;
+use App\User;
 
 $factory->define(Event::class, function (Faker $faker) {
     return [
-        //
+        'title' => $faker->name,
+        'description' => $faker->text(),
+        'start_at' => $faker->dateTimeBetween($startDate = '-2 days', $endDate = 'now', $timezone = null),
+        'end_at' => $faker->dateTimeInInterval($startDate = 'now', $interval = '+ 5 days', $timezone = null),
+        'user_id' => User::all()->random()->id
     ];
 });
