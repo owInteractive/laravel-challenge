27e19ba4cd3b32325e364721a099f172d0d2be47
Jclavo <jclavotafur@gmail.com>
2020-08-10 23:26:31 -0300

    Migration for Event Table

--- database/migrations/2020_08_11_014200_create_events_table.php
+++ database/migrations/2020_08_11_014200_create_events_table.php
@@ -15,6 +15,15 @@ class CreateEventsTable extends Migration
     {
         Schema::create('events', function (Blueprint $table) {
             $table->id();
+            $table->string('title',50);
+            $table->string('description',200);
+            $table->dateTime('start_at',0);
+            $table->dateTime('end_at',0);
+
+            //FK for Invoice Types table
+            $table->unsignedBigInteger('user_id');
+            $table->foreign('user_id')->references('id')->on('users');
+
             $table->timestamps();
         });
     }
