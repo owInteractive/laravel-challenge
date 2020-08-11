3ad03dd4c6ab956666da3a8066b7c77798c6617d
Jclavo <jclavotafur@gmail.com>
2020-08-10 23:27:14 -0300

    add mass assignable in Event Model

--- app/Event.php
+++ app/Event.php
@@ -6,5 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 
 class Event extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title', 'description', 'start_at', 'end_at', 'user_id',
+    ];
 }
