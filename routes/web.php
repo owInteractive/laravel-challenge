8af9e2e61a33cf364dcdd9741e269d5458b3c412
Jclavo <jclavotafur@gmail.com>
2020-08-11 10:36:54 -0300

    add route for Event controller

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,10 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware(['auth'])->group(function () {
+    //Events
+    Route::resource('events', 'EventController');
+});
+
+
