17669674fbe8bb3fdb90d85e51207d509d0c372b
Jclavo <jclavotafur@gmail.com>
2020-08-12 17:48:34 -0300

    migration for table event_mails

---
+++ database/migrations/2020_08_12_193619_create_event_mails_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventMailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('event_mails', function (Blueprint $table) {
+            $table->id();
+            $table->string('email');
+
+            //FK for Invoice Types table
+            $table->unsignedBigInteger('event_id');
+            $table->foreign('event_id')->references('id')->on('events');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('event_mails');
+    }
+}
